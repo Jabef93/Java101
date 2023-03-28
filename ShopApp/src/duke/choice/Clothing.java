4c1b8b0f0d50ac603cefbc4f1124a5951331d1d7
Jabef93 <josebarriga01@gmail.com>
2023-03-28 10:55:22 -0300

    Encapsulamiento clases Clothing y Customer

--- ShopApp/src/duke/choice/Clothing.java
+++ ShopApp/src/duke/choice/Clothing.java
@@ -1,7 +1,45 @@
 package duke.choice;
 
 public class Clothing {
-	String description;
-	double price;
-	String size = "M";
+	private String description;
+	private double price;
+	private String size = "M";
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	
 }
--- ShopApp/src/duke/choice/Customer.java
+++ ShopApp/src/duke/choice/Customer.java
@@ -1,6 +1,32 @@
 package duke.choice;
 
 public class Customer {
-	String name;
-	String size;
+	private String name;
+	private String size;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+
 }
